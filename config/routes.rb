d99a9f97b4cf6c3a81e95a6a76c341b515003a27
nhson2022 <sonpgdlh@gmail.com>
2023-02-04 13:29:39 +0700

    Them view index va show Article

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "/info" => "pages#info", as: :info
+
   devise_for :users
+  # For Admin user
   namespace :admin do
     resources :articles
+  end
+  # For normal user
+  resources :articles, only: [] do
+    get "/" => "articles#index", on: :collection, as: :list
+    get "/:id" => "articles#show", on: :collection, as: :detail
   end
 end
